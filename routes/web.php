d780b0534ebe400751972952ce6727d7dbf0a5b2
Spyros <spiros@jobsinkent.com>
2024-01-22 12:22:16 +0000

    update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('layouts.layout');
+//});
+
+Route::get('/', [LoginController::class, 'index']);
+
+Route::post('auth', [LoginController::class, 'auth'])->name('auth');
